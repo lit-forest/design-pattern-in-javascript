6713d1353ad35dbd18f5dc2248941efcab5de49c
Sylvenas <sylvenas@outlook.com>
2018-01-29 15:45:24 +0800

    构造函数模式

--- test/test.js
+++ test/test.js
@@ -1,70 +1,14 @@
-function Animal(type) {
+function Dog(name, type) {
+	this.name = name;
 	this.type = type;
+	this.sound = sound;
 }
 
-Animal.prototype.move = function () {
-	console.log('I am moving');
+function sound(val) {
+	console.log(val);
 }
 
-var animal = new Animal('dog')
+var xiaobu = new Dog('小布', '拉布拉多');
+var erhuo = new Dog('傻子', '哈士奇');
 
-console.log(animal.__proto__ === Animal.prototype);    // true
-console.log(animal.type);                              // dog
-console.log(animal.move())							   // 'I am moving'
-
-console.log(Animal.prototype.constructor === Animal)   // true
-
-console.log(Animal.__proto__ === Function.prototype)  // true
-
-console.log(Animal.prototype.__proto__ === Object.prototype)  // true
-
-function Dog() {
-
-}
-
-Dog.prototype = new Animal();
-Dog.prototype.constructor = Dog;
-
-Dog.prototype.__proto__ === Animal.prototype
-
-const newObj = Object.create({ a: 1 }, { b: { value: 1, writable: false, configurable: true } })
-console.log(newObj);
-console.log(newObj.__proto__);
-
-
-function Animal(type) {
-	this.type = type;
-}
-
-Animal.prototype.move = function () {
-	console.log('I am moving');
-}
-
-function Dog(type) {
-	Animal.call(this, type);
-}
-
-Dog.prototype = Object.create(Animal.prototype);
-Dog.prototype.constructor = Dog;
-
-const dog = new Dog('金毛');
-console.log(dog.type);
-console.log(dog.move());
-
-
-
-function MyClass() {
-	SuperClass.call(this);
-	OtherSuperClass.call(this);
-}
-
-// inherit one class
-MyClass.prototype = Object.create(SuperClass.prototype);
-// mixin another
-Object.assign(MyClass.prototype, OtherSuperClass.prototype);
-// re-assign constructor
-MyClass.prototype.constructor = MyClass;
-
-MyClass.prototype.myMethod = function () {
-	// do a thing
-};
+xiaobu.sound === erhuo.sound;
