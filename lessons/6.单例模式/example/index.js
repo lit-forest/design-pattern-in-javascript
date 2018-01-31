b852534a558e216c66927253b0212126a0eb264c
Sylvenas <sylvenas@outlook.com>
2018-01-31 20:17:50 +0800

    单例模式

---
+++ lessons/6.单例模式/example/index.js
@@ -0,0 +1,23 @@
+// 单利模式即只创建一个实例对象，在一个项目中utils要么不出现，要么只能出现一个
+var utils = {
+	formatDate: function () { },
+	ajax: function () { },
+	curry: function () { }
+}
+
+// 高级一点的只创建一个实例，在需要的时候创建
+var getCEO = (function () {
+	var instance;
+	var createCEO = function () {
+		return instance = {
+			name: '霸道总裁',
+			age: '35',
+			sex: 'male'
+		}
+	}
+	return {
+		getInstance: function () {
+			return instance || (instance = createCEO())
+		}
+	}
+}())
